eb19b6d53ec0d96e45c74244bd1431f54924f279
ann <kr>
2019-04-13 16:45:45 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ HeroIcon.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HeroIcon extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>{this.props}</div>
+        )
+    }
+}
+
+export default HeroIcon;
